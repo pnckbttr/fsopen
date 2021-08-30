061f0240730e767dec340e3e5c56782a1badb80b
pnckbttr <pnckbttr@protonmail.com>
2021-08-30 09:57:16 +0200

    seperate buttons

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,20 +1,51 @@
 import React, { useState } from 'react'
 
+const Display = (props) => {
+        return (
+            <div>{props.counter}</div>
+        )
+    }
+
+const Button = (props) => {
+        return (
+            <button onClick={props.onClick}>
+                {props.text}
+            </button>
+        )
+    }
+
+
+
 const App = () => {
+
+
     const [ counter, setCounter ] = useState(0)
+
+    const increaseByOne = () => setCounter(counter + 1)
+
+    const decreaseByOne = () => setCounter(counter -1)
+
+    const setToZero = () => setCounter(0)
 
     console.log('clicked')
     console.log('render..', counter)    
 
     return (
         <div>
-            <div>{counter}</div>
-            <button onClick={() => setCounter(counter + 1)}>
-                plus
-            </button>
-            <button onClick={() => setCounter(0)}>
-                zero
-            </button>
+            <Display counter={counter}/>
+            <Button
+            onClick={increaseByOne}
+            text='plus'
+            />
+            <Button
+            onClick={decreaseByOne}
+            text='minus'
+            />
+            <Button
+            onClick={setToZero}
+            text='zero'
+            />
+
             </div>
     )
 }
