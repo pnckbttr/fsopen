867374c1cc29d35a643f3e23ea1a5ca37c174996
pnckbttr <pnckbttr@protonmail.com>
2021-08-31 12:26:58 +0200

    restructured directories

---
+++ part1/unicafe/1.8/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+
+const Display = (props) => (
+    <h2>
+        {props.text}
+    </h2>
+)
+
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+        {props.text}
+    </button>
+)
+
+const Statistics = (props) => {
+
+    return (
+    <div>
+        good {props.good}
+        <br/>
+        neutral {props.neutral}
+        <br/>
+        bad {props.bad}
+        <br/>
+        all {props.all}
+        <br/>
+        average {props.avg}
+        <br/>
+        positive {props.pos} %
+
+    </div>
+    )
+}
+
+const App = () => {
+
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    const all = good + neutral + bad
+    const avg = (good - bad) / all
+    const pos = good / all
+
+    const addGood = () => {
+        setGood(good + 1)
+    }
+
+    const addNeutral = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const addBad = () => {
+        setBad(bad + 1)
+    }
+
+    return (
+        <div>
+            
+            <Display text='give feedback'/>
+            <Button handleClick={addGood} text='good'/>
+            <Button handleClick={addNeutral} text='neutral'/>
+            <Button handleClick={addBad} text='bad'/>
+            <Display text='statistics'/>
+            <Statistics good={good} neutral={neutral}
+                        bad={bad} all={all}
+                        avg={avg} pos={pos}/>
+            
+
+        </div>
+    )
+}
+
+
+export default App
