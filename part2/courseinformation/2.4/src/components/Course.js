a9e40494535e2b91f72bbd3c18697e64c0301069
pnckbttr <pnckbttr@protonmail.com>
2021-09-03 01:32:09 +0200

    optimized 2.4/2.5

--- part2/courseinformation/2.4/src/components/Course.js
+++ part2/courseinformation/2.4/src/components/Course.js
@@ -1,45 +1,46 @@
 import React from 'react'
 
+
 const Course = ({courses}) => {
 
-    const Content = () => {
+    const reducer = (totalExercises, currentExercise) => 
+            totalExercises + currentExercise
+           
+    const content = courses.map(course =>
 
-        const contentArray = [] 
+        <div key={course.id}>
+            
+            <h3 key={course.id}>{course.name}</h3> 
 
-        for (const course of courses) {
+        {course.parts.map(part =>
 
-            const courseTitle = <h3 key={course.id}>{course.name}</h3>
+            <div key={part.id}>
 
-            contentArray.push(courseTitle)
+            <p key={part.id}>{part.name} {part.exercises}</p>
+            
+            </div>
 
-            let number = 0
-
-            for (const part of course.parts) {
-
-                const coursePart =
-                <p key={part.name}>{part.name} {part.exercises}</p>
-
-                contentArray.push(coursePart)
-
-                number += part.exercises          
+             )
             }
-            const total = 
-            <p key={number}><b>total of {number} exercises</b></p>
 
-            contentArray.push(total)
-        }
-        return (
-            contentArray
+        <b>
+            total of {
+
+            courses[course.id-1].parts.map(part =>
+            part.exercises).reduce(reducer)
+
+            } exercises
+        </b> 
+
+        </div>     
+    )
+
+    return (  
+        <div>
+            {content}
+        </div>    
         )
     }
-
-    return (
-        
-        <Content/>
-        
-        )
-    }
-
 
 
 export default Course
